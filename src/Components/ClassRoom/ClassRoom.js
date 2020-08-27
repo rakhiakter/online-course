97f5b661b86ad68fdc9602fb9eda0dd1f5f4373c
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-08-28 01:48:28 +0600

    code added

--- src/Components/ClassRoom/ClassRoom.js
+++ src/Components/ClassRoom/ClassRoom.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import fakeData from '../../FakeData/data'
+import fakeData from '../../FakeData/data';
+import './ClassRoom.css';
 const ClassRoom = () => {
   console.log(fakeData);
-  return (
-    <div>
-      <h1>this is class</h1>
+  const data = fakeData;
+  // const [courses, setCourses] = useState(data);
+    return (
+    <div className="class-container">
+    <div className="course-container">
+    <ul>
+       {
+         data.map(d => 
+          <li>{d.Title}</li>
+          )
+       }
+     </ul>
+    </div>
+    <div className="cart-container">
+      <h3>this is cart</h3>
+    </div>
     </div>
   );
 };
